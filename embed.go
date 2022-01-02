2895e97ea3d9dce248acf73c82085a5064360226
cnwangfei <96471220+cnwangfei@users.noreply.github.com>
2022-01-03 00:00:55 +0800

    Add files via upload

---
+++ embed.go
@@ -0,0 +1,23 @@
+package httpEmbed
+
+import (
+	"embed"
+	"io/fs"
+	"path"
+	"path/filepath"
+)
+
+type Fs struct {
+	*embed.FS        // 静态资源
+	Path      string // 设置embed文件到静态资源的相对路径，也就是embed注释里的路径
+}
+
+func (f Fs) Open(name string) (fs.File, error) {
+	// 拼接相对路径
+	fullName := filepath.Join(f.Path, filepath.FromSlash(path.Clean("/"+name)))
+	// 修改http路径中的/为\
+	fullName = filepath.ToSlash(fullName)
+	//fmt.Println(fullName)
+	fs, err := f.FS.Open(fullName)
+	return fs, err
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cnwangfei/go-HttpEmbed
+
+go 1.16
